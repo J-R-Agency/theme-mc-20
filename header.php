5e36af781899e8770a33d970a9f308966f0eee3a
Joy Ling <joy@joys-mbp.home>
2020-05-05 12:23:10 +0100

    Created header

--- header.php
+++ header.php
@@ -40,49 +40,42 @@ $container = get_theme_mod( 'understrap_container_type' );
 			<div class="container">
 		<?php endif; ?>
 
-					<!-- Your site title as branding in the menu -->
-					<?php if ( ! has_custom_logo() ) { ?>
+			<!-- Your site title as branding in the menu --> 
+			<div class='site-logo'>
+				<a href="<?php echo get_site_url(); ?>">
+					<img class="logo-<?php echo $bw; ?>-<?php echo $page_color; ?>"
+					src="<?php echo get_template_directory_uri(); ?>
+					/assets/logos/logo-<?php echo $bw; ?>-<?php echo $page_color; ?>.png"
+					alt="Matchstick Creative Logo">
+				</a>
+			</div>		
+			
+			<button class="hamburger" type="button">
+				<img class='menu-burger-<?php echo $page_color; ?>' src="<?php echo get_template_directory_uri(); ?>
+					/assets/menu/menu-burger-<?php echo $page_color; ?>.png">
+			</button>
+			<button class="close" type="button">
+				<img class='menu-close-<?php echo $page_color; ?>' src="<?php echo get_template_directory_uri(); ?>
+					/assets/menu/menu-close-<?php echo $page_color; ?>.png">
+			</button>
 
-						<?php if ( is_front_page() && is_home() ) : ?>
-
-							<h1 class="navbar-brand mb-0"><a rel="home" href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" itemprop="url"><?php bloginfo( 'name' ); ?></a></h1>
-
-						<?php else : ?>
-
-							<a class="navbar-brand" rel="home" href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" itemprop="url"><?php bloginfo( 'name' ); ?></a>
-
-						<?php endif; ?>
-
-						<?php
-					} else {
-						the_custom_logo();
-					}
-					?>
-					<!-- end custom logo -->
-
-				<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="<?php esc_attr_e( 'Toggle navigation', 'understrap' ); ?>">
-					<span class="navbar-toggler-icon"></span>
-				</button>
-
-				<!-- The WordPress Menu goes here -->
-				<?php
-				wp_nav_menu(
-					array(
-						'theme_location'  => 'primary',
-						'container_class' => 'collapse navbar-collapse',
-						'container_id'    => 'navbarNavDropdown',
-						'menu_class'      => 'navbar-nav ml-auto',
-						'fallback_cb'     => '',
-						'menu_id'         => 'main-menu',
-						'depth'           => 2,
-						'walker'          => new Understrap_WP_Bootstrap_Navwalker(),
-					)
-				);
-				?>
 			<?php if ( 'container' === $container ) : ?>
 			</div><!-- .container -->
 			<?php endif; ?>
-
+			
 		</nav><!-- .site-navigation -->
-
+			<div id='mega-menu'>
+						<!-- The WordPress Menu goes here -->
+						<?php
+						wp_nav_menu(
+							array(
+								'theme_location'  => 'primary',
+								'fallback_cb'     => '',
+								'menu_id'         => 'main-menu',
+								'depth'           => 2,
+								'walker'          => new Understrap_WP_Bootstrap_Navwalker(),
+							)
+						);
+						?>
+			</div>
 	</div><!-- #wrapper-navbar end -->
