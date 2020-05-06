d6d37bca783de20a8ad9965a554f48d42ce75d51
Joy Ling <joy@joys-mbp.home>
2020-05-06 16:37:36 +0100

    Added hero filter and text

--- global-templates/template-parts/hero.php
+++ global-templates/template-parts/hero.php
@@ -12,7 +12,14 @@ $theme_path = get_template_directory_uri();
 
 <?php if ( has_post_thumbnail() ): ?>
 	<?php $thumb = get_the_post_thumbnail_url(); ?>
-	<?php $featured_image_position = get_field('featured_image_position');?>
+	<?php
+		$featured_image_position = get_field('featured_image_position');
+		$hero = get_field('hero');
+	?>
+	<div class='hero-filter <?php the_field('hero_filter');?>'>
+		<h1><?php echo strip_tags($hero['hero_title'],'<span>'); ?></h1>
+		<p><?php echo strip_tags($hero['hero_subtitle'],'<span>'); ?></p>
+	</div>
 	<div class="hero"
 		 style="background-image: url('<?php echo $thumb; ?>');
 				background-position:
