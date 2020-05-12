49e1301867f207560e65a370cc7ec8ec157ee9d6
Joy Ling <joy@joys-mbp.home>
2020-05-12 11:31:27 +0100

    Added icon set, CTA block

--- loop-templates/content-flexible.php
+++ loop-templates/content-flexible.php
@@ -57,6 +57,7 @@ if( have_rows('flexible_content_block') ):
             $isb_title = get_sub_field('isb_title'); // Image
             $isb_icons = get_sub_field('isb_icons'); // Select 
 			
+			
 			echo "<section class='generic ".$isb_background_color." icon-set'>
 						<h2>".strip_tags($isb_title,'<span>')."</h2>
 							<div class='icon-set-container'>";
@@ -83,9 +84,61 @@ if( have_rows('flexible_content_block') ):
 							endif;
 							
 			echo	"</div>
-				  </section>";
+				  </section>";	
+
+
+
+          // -------------------------- //
+         // ----- CASE: CTA BLOCK -----//
+        // -------------------------- //
+        elseif( get_row_layout() == 'cta_block' ):
+
+            $cta_style = get_sub_field('cta_style');
+            $cta_filter = get_sub_field('cta_filter');
+            $cta_title = get_sub_field('cta_title');
+            $cta_intro = get_sub_field('cta_intro');
+            $cta_link = get_sub_field('cta_link');
+			$cta_image = get_sub_field('cta_image');
 			
-		endif;		
+				echo "
+				<div class='cta-filter ".$cta_filter."'>";
+				
+				if ($cta_style == 'secondary'):
+					echo "<div class='cta-post-it'>";
+				endif;
+				
+				echo "<h1>".$cta_title."</h1>
+					 <p>".$cta_intro."</p>
+					 <div class='button-navy'><a href='".$cta_link['url']."'>".$cta_link['title']."</a></div>";
+				
+				if ($cta_style == 'secondary'):
+					echo "</div>";
+				endif;
+					 
+				echo "
+				</div>
+				<div class='cta-block' style='background-image:url(".$cta_image['url'].")'>
+					
+				</div>
+				";
+
+          // --------------------------- //
+         // - CASE: CASE STUDIES BLOCK -//
+        // --------------------------- //
+        elseif( get_row_layout() == 'case_studies_block' ):
+
+            $csb_title = get_sub_field('csb_title');
+			
+			echo "
+			<section class='generic bg-white'>
+				<h2>".$csb_title."</h2>
+			
+			</section>
+			";
+
+			
+		endif; // Final endif
+
                     
     // End loop.
     endwhile;
