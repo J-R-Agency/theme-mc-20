62b9930136a5951129c2b5c205353ee3e46a21a0
Joy Ling <joy@joys-mbp.home>
2020-05-13 16:45:51 +0100

    Worked on case study template

--- page-templates/case-study.php
+++ page-templates/case-study.php
@@ -12,11 +12,28 @@
 defined( 'ABSPATH' ) || exit;
 
 $bw = 'black';
-$page_color = 'cyan';
+$page_color = 'green';
 $header_position = get_field('header_position');
 
 include( locate_template( 'header.php', false, false ) );  ?>
 
-<?php include( locate_template( 'loop-templates/content-flexible.php', false, false ) );  ?>
+<!-- QUOTE -->
+<?php if( have_rows('cs_quote') ): ?>
+    <?php while( have_rows('cs_quote') ): the_row(); 
+
+        // Get sub field values.
+        $csq_quote = get_sub_field('csq_quote');
+        $csq_citation = get_sub_field('csq_citation');
+
+        ?>
+        <section class='cs-quote-block'>
+	        <div class='cs-content'>
+		    	<h3><?php echo strip_tags($csq_quote); ?></h3>
+				<p><?php echo strip_tags($csq_citation); ?></p>
+	        </div>
+        </section>
+        
+    <?php endwhile; ?>
+<?php endif; ?>
 
 <?php include( locate_template( 'footer.php', false, false ) ); ?>
