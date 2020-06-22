fbae8befaadcbe10c0775524299c0b8d1b688eef
Joy Ling <joy@Joys-MacBook-Pro.local>
2020-06-22 16:39:26 +0100

    Blog card fix

--- global-templates/template-parts/helpful-stuff-categories.php
+++ global-templates/template-parts/helpful-stuff-categories.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Case study card
+ * Helpful Stuff Categories
  *
  * @package understrap
  */
@@ -10,31 +10,38 @@ defined( 'ABSPATH' ) || exit;
 $theme_path = get_template_directory_uri();
 ?>
 
-<?php if( have_rows('downloads') ): ?>
-	<?php while( have_rows('downloads') ): the_row(); 
-
-		// vars
-		$file_name = get_sub_field('file_name');
-		$image = get_sub_field('image');
-		$file = get_sub_field('file');
-		$color = get_sub_field('color');
-		
-		?>
-		
-		<div class="download-card-container">
-			<a href="<?php echo $file['url']; ?>" download>
-				<div class='download-card-header <?php echo $color; ?>'>
-					<h3><?php echo $file_name; ?></h3>
-				</div>
-				<div class='download-card-img' style="background-image:url(<?php echo $image['url']; ?>)"></div>
+<section class='generic bg-white'>
+	<h2>Meet our <span class='underline-cyan-2' style='padding-bottom: 15px;'>#Femalefounders</span></h2>
+	<p class='hs-subtitle'>Incredible businesses, organisations and projects run by women</p>
+	
+	<div class='case-studies-container'>
+	<?php		
+		$args = array(
+		    'post_type'      => 'post', //write slug of post type
+		    'posts_per_page' => 3,
+		    'order'          => 'DESC',
+		    'category__in'   => $female_founders
+		 );
+		 
+		$ffquery = new WP_Query( $args );
+		 
+		if ( $ffquery->have_posts() ) :
+		 
+		    while ( $ffquery->have_posts() ) : $ffquery->the_post();
+			 	
+				$mc_img = wp_get_attachment_url( get_post_thumbnail_id($post->ID) );
+				$hover_color = 'cyan';
+				$categories = get_the_category();
 				
-				<div class='ribbon-wrapper'>
-					<div class='download-card-ribbon <?php echo $color; ?>'></div>
-				</div>
-			</a>
-		</div>
-		
-		
-
-	<?php endwhile; ?>
-<?php endif; ?>
+				include (get_template_directory().'/global-templates/template-parts/blog-card-small.php');	
+			
+			endwhile;
+		endif; 
+		wp_reset_query();						
+						
+	?>
+	</div>
+</section>
+<section class='link-block bg-light-grey'>
+	<a href='<?php echo site_url();?>/category/female-founders'>Meet more #femalefounders<div class='arrow-link-cyan'></div></a>
+</section>
