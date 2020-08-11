b8bac40f9ae939af4aa434bce8f970571756687f
Joy Ling <joy@Joys-MBP.lan>
2020-08-11 16:18:34 +0100

    Case study tabs fix

--- global-templates/template-parts/case-study-tabs.php
+++ global-templates/template-parts/case-study-tabs.php
@@ -13,17 +13,29 @@ $theme_path = get_template_directory_uri();
 <div class='case-studies-header'>
 	<h2 id='cs-title'>More Inspiring Stories</h2>
 	<?php
-		echo '<ul class="nav nav-tabs cs-categories-list" role="tablist">
-	    		<li>
-	                <a class="tab" id="tab-all" class="active" href="#all" role="tab" data-toggle="tab">All</a>
-	            </li>
-	    
+		
+		echo '
+		<ul class="nav nav-tabs cs-categories-list" role="tablist">
+    		<li>
+                <a class="tab" id="tab-all" class="active" href="#all" role="tab" data-toggle="tab">All</a>
+            </li>
 	    ';
+	    
+	    $content = get_cat_ID('content');
+	    $experience = get_cat_ID('experience');
+	    $full_service = get_cat_ID('full service');
+	    $identity = get_cat_ID('identity');
+	    
+	    $included_cats = array( $content, $experience, $full_service, $identity );
+	    
 	    $args = array(
 	        'orderby' => 'name',
-	        'order' => 'ASC'
+	        'order' => 'ASC',
+	        'include' => $included_cats
 	    );
+	    
 	    $categories = get_categories($args);
+	    
 	    foreach($categories as $category) { 
 	        echo 
 	            '<li>
