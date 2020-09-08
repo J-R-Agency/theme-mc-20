b5b82c66c6ff4857e56937060dab67d6caa887f1
Joy Ling <joy@Joys-MacBook-Pro.local>
2020-09-08 15:52:41 +0100

    Blog card image quality adjustment

--- global-templates/template-parts/blog-card.php
+++ global-templates/template-parts/blog-card.php
@@ -44,7 +44,7 @@ $category_color = get_field('category_color', $categories[0]);
 	<div class='blog-card-right'>
 		<a href="<?php the_permalink(); ?>">
 			<?php if ( has_post_thumbnail() ) {
-		    		echo "<img src=\"" . get_the_post_thumbnail_url($post->ID, 'large') . "\"
+		    		echo "<img src=\"" . get_the_post_thumbnail_url($post->ID, 'full') . "\"
 		    				style='object-position:".$featured_image_position.";'>";
 				} else { 
 					echo "<img src='". get_template_directory_uri() ."/assets/images/blog-card-placeholder.jpg'>";
