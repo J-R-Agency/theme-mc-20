45602f5c0a61df4f2a87a015f8e5f13b56af3343
Joy Ling <joy@joys-mbp.home>
2021-02-03 11:25:07 +0000

    Changed page icon sizes to medium

--- page-templates/content.php
+++ page-templates/content.php
@@ -43,9 +43,9 @@ include( locate_template( 'header.php', false, false ) );  ?>
 			 		<a href='",the_permalink(),"'>
 			 	";
 			 	if ($page_icon) {
-				 	echo "<img class='small' src='".$page_icon['url']."' alt='".$page_icon['alt']."'>";
+				 	echo "<img class='medium' src='".$page_icon['url']."' alt='".$page_icon['alt']."'>";
 			 	} else {
-				 	echo "<img class='small' src='".get_template_directory_uri()."/assets/graphics/placeholder-icon.png'>";
+				 	echo "<img class='medium' src='".get_template_directory_uri()."/assets/graphics/placeholder-icon.png'>";
 			 	}
 			 	the_title("<h5>","</h5>");
 			 	
