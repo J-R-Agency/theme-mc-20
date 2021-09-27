b859214f9870da818073ceca5967029da5957f24
Joy Ling <joy@matchstickcreative.co.uk>
2021-09-27 11:36:17 +0000

    Removed extraneous templates

--- archive.php
+++ archive.php
@@ -1,8 +1,6 @@
 <?php /**
- * Template Name: Blog Archive Template
  *
- * Template for displaying a page just with the header and footer area and a "naked" content area in between.
- * Good for landingpages and other types of pages where you want to add a lot of custom markup.
+ * Template for displaying all blog posts
  *
  * @package understrap
  */
--- category.php
+++ category.php
@@ -1,8 +1,5 @@
 <?php /**
- * Template Name: Category Blog Archive Template
- *
- * Template for displaying a page just with the header and footer area and a "naked" content area in between.
- * Good for landingpages and other types of pages where you want to add a lot of custom markup.
+ * Template for displaying category blog posts
  *
  * @package understrap
  */
