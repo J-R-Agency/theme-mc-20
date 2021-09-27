0b4d22e42516508d113bc6f85ab505be3b8aeeaa
Joy Ling <joy@matchstickcreative.co.uk>
2021-09-27 11:25:37 +0000

    Changed case study template to case study cpt only

--- page-templates/case-study.php
+++ page-templates/case-study.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * Template Name: Case Study Template
- * Template Post Type: page, case-studies
+ * Template Post Type: case-studies
  *
  * Template for displaying a page just with the header and footer area and a "naked" content area in between.
  * Good for landingpages and other types of pages where you want to add a lot of custom markup.
